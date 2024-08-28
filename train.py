5ccd098380e917b612595e2d050daa6cb971f9eb
jinwoo <jinwoolover@gmail.com>
2024-08-28 16:00:11 +0900

    feat: apply argparse

--- train.py
+++ train.py
@@ -1,23 +1,48 @@
-from transformers import Trainer, DataCollatorWithPadding
+import argparse
+import os
+
+from transformers import (AutoModelForSequenceClassification,
+                          DataCollatorWithPadding, Trainer)
+
+from callback import LossCallback
 from config import get_training_arguments
 from dataset import load_and_tokenize_dataset
-from utils import compute_metrics, seed_everything, plot_confusion_matrix
-from transformers import AutoModelForSequenceClassification
-from callback import LossCallback
+from utils import compute_metrics, plot_confusion_matrix, seed_everything
 
 
 def train(output_dir='results'):
-    seed_everything()
-    datasets, tokenizer = load_and_tokenize_dataset()
+    parser = argparse.ArgumentParser()
     
-    model = AutoModelForSequenceClassification.from_pretrained(
-        'klue/bert-base', num_labels=datasets['train'].features['label'].num_classes
-    )
-    
+    parser.add_argument("--seed", type=int, required=False, default=42)
+    parser.add_argument("--epoch", type=int, required=False, default=3)
+    parser.add_argument("--model", type=str, required=False, default="klue/bert-base")
+    parser.add_argument("--plot_name", type=str, required=False, default='plot')
+    parser.add_argument("--batch_size", type=int, required=False, default=32)
+    parser.add_argument("--result_name", type=str, required=False, default='result')
+    parser.add_argument("--logging_interval", type=int, required=False, default=30)
+
+    args = parser.parse_args()
+
+    seed_everything(args.seed)
+
+    base_dir = os.path.join(os.getcwd(), 'plots')
+    loss_plot_dir = os.path.abspath(os.path.join(base_dir, 'loss'))
+    cfm_dir = os.path.abspath(os.path.join(base_dir, 'cfm'))
+    result_dir = os.path.abspath(os.path.join(base_dir, 'result', args.result_name))
+
+    os.makedirs(loss_plot_dir, exist_ok=True)
+    os.makedirs(cfm_dir, exist_ok=True)
+
+    datasets, tokenizer = load_and_tokenize_dataset(tokenizer=args.model)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    training_args = get_training_arguments()
-    loss_callback = LossCallback(logging_interval=30)
-    
+    loss_callback = LossCallback(logging_interval=args.logging_interval)
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model, num_labels=datasets['train'].features['label'].num_classes
+    )
+    training_args = get_training_arguments(
+        output_dir=result_dir, epochs=args.epoch, batch_size=args.batch_size
+    )
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -29,11 +54,11 @@ def train(output_dir='results'):
     )
     
     trainer.train()
-    loss_callback.plot_loss()
+    loss_callback.plot_loss(loss_plot_dir + '/loss_')
     
     results = trainer.evaluate()
     predictions = trainer.predict(datasets["test"])
-    plot_confusion_matrix(predictions)
+    plot_confusion_matrix(predictions, output=cfm_dir + '/cfm_')
     
     
 if __name__ == "__main__":
