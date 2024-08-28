33f44134d02621347b0753cb5b0ae4bafa2b289a
jinwoo <jinwoolover@gmail.com>
2024-08-28 15:59:15 +0900

    refactor: add type hint

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
+from typing import Optional
+
 from transformers import TrainingArguments
 
 
-def get_training_arguments():
+def get_training_arguments(
+        output_dir = Optional[str],
+        epochs = Optional[int],
+        batch_size = Optional[int],
+) -> TrainingArguments:
     return TrainingArguments(
-        output_dir='model',
-        num_train_epochs=2,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=8,
+        output_dir=output_dir,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         learning_rate=5e-5,
         lr_scheduler_type='cosine',
         weight_decay=0.01,
